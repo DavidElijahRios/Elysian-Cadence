520e922b15e04edcd23eec30614494eb0f992c1d
David Rios <david.riostech@outlook.com>
2022-11-06 19:19:14 -0800

    Finished adding links for social media and also streaming links and got them attached to the proper sites. Also styled music section and added album art. Now ready to make main body of home page and style it then can move on to the rest of the pages.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./index.css";
 import React from 'react';
 // need ability to write routes for web page
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// TODO: Need to import header and footer.js for it to appear in app.js
 import Header from "./pages/header/header.js"
 import Home from "./pages/home/home.js"
 import Footer from "./pages/footer/footer.js"
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,24 @@
 import React from "react";
-import "./home.css"
+import "./home.css";
+import aeona from '../../images/AEONA.jpg';
+import revelation from '../../images/REVELATION.jpg';
+import brokenMachine from '../../images/BROKEN_MACHINE.JPG';
+import keepOn from '../../images/KEEP_ON.JPG';
 
 export default function Home() {
     return (
         <div>
             <h1>Test Test</h1>
+            <p> This will be other content and white space</p>
+            <div id="music">
+              <h2> Music </h2>
+              <ul>
+              <img className="single-art" src={aeona} alt="Aeona"/>
+              <img className="single-art" src={revelation} alt="revelation"/>
+              <img className="single-art" src={brokenMachine} alt="brokenMachine"/>
+              <img className="single-art" src={keepOn} alt="keepOn"/>
+              </ul>
+            </div>
         </div>
     )
 }
