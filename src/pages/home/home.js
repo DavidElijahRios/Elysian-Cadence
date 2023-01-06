85393f8009e908543c0a3d692b3e97ba89376201
David Rios <david.riostech@outlook.com>
2023-01-05 16:04:05 -0800

    Was able to sucessfully add carousel animation to web page but just need to fix some minor bugs with it.

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -17,41 +17,23 @@ import { faSpotify, faApple, faAmazon, faYoutube } from '@fortawesome/free-brand
 // ?PLAN: I may switch auto carousel idea to scrollable slide show with animation to opaque album art section with a button to switch to music page and create a full out music page
 
 
-// const data = [ aeona, revelation, brokenMachine, keepOn ]
-// const Carousel = () => {
-//     const [ currentIndex, setCurrentIndex ] = useState(0)
-//        const scroll = () => {
-//             if(currentIndex === data.length-1) {
-//                 return setCurrentIndex(0)
-//             } else {
-//                 return setCurrentIndex(currentIndex+1)
-//             }
-//        }
-//        useEffect(() => {
-//         const interval = setInterval(() => {scroll()}, 3000)
-
-//         return () => clearInterval(interval)})
-
-// }
-
-
 
 
 export default function Home() {
 
-    const data = [ aeona, revelation, brokenMachine, keepOn]
-        const [ currentIndex, setCurrentIndex ] = useState(0)
-           const scroll = () => {
-                if(currentIndex === data.length-1) {
-                    return setCurrentIndex(0)
-                } else {
-                    return setCurrentIndex(currentIndex+1)
-                }
-           }
-           useEffect(() => {
-            const interval = setTimeout(() => {scroll()}, 1000)
+    // const data = [ aeona, revelation, brokenMachine, keepOn, aeona, revelation]
+    //     const [ currentIndex, setCurrentIndex ] = useState(0)
+    //        const scroll = () => {
+    //             if(currentIndex === data.length-1) {
+    //                 return setCurrentIndex(0)
+    //             } else {
+    //                 return setCurrentIndex(currentIndex+1)
+    //             }
+    //        }
+    //        useEffect(() => {
+    //         const interval = setTimeout(() => {scroll()}, 1000)
     
-            return () => clearTimeout(interval)})
+    //         return () => clearTimeout(interval)})
     
 
 
@@ -71,28 +53,58 @@ export default function Home() {
             </div>
                 
                 {/* Music Carousel will go here */}
-            <div id="music">
-                <h2> Music </h2>
-                <div className="inner-music">
-                    <ul className="music-container">
-                        {/* mapping through Data so that i can display all available album releases in data Array */}
-                        { data.map((item, index) => {
+                        {/* Line 60 under music-container { data.map((item, index) => {
                             return (
                                 <img className="single-art" style={{transform: `translate(-${currentIndex}%)`}} src={item} alt="photo" />
                             ) 
-                        })}
-                        {/* <div className="music-item">
-                            <img className="single-art" src={revelation} alt="revelation" />
-                        </div>
-                        <div className="music-item">
-                            <img className="single-art" src={brokenMachine} alt="brokenMachine" />
-                        </div>
-                        <div className="music-item">
-                            <img className="single-art" src={keepOn} alt="keepOn" />
-                        </div> */}
-                    </ul>
+                        })} */}
+                        {/* !LEft OFF HERE */}
+            <div id="music-slider">
+                <h2 className="music-header"> Music </h2>
+                <div className="music-slideTrack">
+                    <div className="music-slideItem">
+                        <img className="single-art" src={aeona} alt="revelation" />
+                    </div>
+                    <div className="music-slideItem">
+                        <img className="single-art" src={revelation} alt="revelation" />
+                    </div>
+                    <div className="music-slideItem">
+                        <img className="single-art" src={brokenMachine} alt="revelation" />
+                    </div>
+                    <div className="music-slideItem">
+                        <img className="single-art" src={keepOn} alt="revelation" />
+                    </div>
+                    <div className="music-slideItem">
+                        <img className="single-art" src={aeona} alt="revelation" />
+                    </div>
+                    <div className="music-slideItem">
+                        <img className="single-art" src={revelation} alt="revelation" />
+                    </div>
+                    <div className="music-slideItem">
+                        <img className="single-art" src={brokenMachine} alt="revelation" />
+                    </div>
+                    <div className="music-slideItem">
+                        <img className="single-art" src={keepOn} alt="revelation" />
+                    </div>
+                    <div className="music-slideItem">
+                        <img className="single-art" src={aeona} alt="revelation" />
+                    </div>
+                    <div className="music-slideItem">
+                        <img className="single-art" src={revelation} alt="revelation" />
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
+{/* under inner-music <ul className="music-container">
+    <div className="music-item">
+        <img className="single-art" src={revelation} alt="revelation" />
+    </div>
+    <div className="music-item">
+        <img className="single-art" src={brokenMachine} alt="brokenMachine" />
+    </div>
+    <div className="music-item">
+        <img className="single-art" src={keepOn} alt="keepOn" />
+    </div>
+</ul> */}
