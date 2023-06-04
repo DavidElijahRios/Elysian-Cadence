d9a19fa91f5e0fbd0767c0777abae5de1ce167da
David Rios <david.riostech@outlook.com>
2023-06-03 20:10:27 -0700

    Added new photos fixed the carosel on homepage and added a few functioning links

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,110 +1,71 @@
 import React from "react";
-import { useEffect, useState } from "react";
 import "./home.css";
+import { Link } from "react-router-dom";
 import aeona from '../../images/AEONA.jpg';
 import revelation from '../../images/REVELATION.jpg';
 import brokenMachine from '../../images/BROKEN_MACHINE.jpg';
 import keepOn from '../../images/KEEP_ON.jpg';
-import hero from '../../images/ECHomepage.jpg';
+import NewHorizon from '../../images/New-horizon.jpg';
+import hero from '../../images/ECHomepage.JPG';
 // Need to bring in font awesome to be able to apply icons into button 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpotify, faApple, faAmazon, faYoutube } from '@fortawesome/free-brands-svg-icons';
-
-// TODO: need to create a a scrolling carrousel for music section
-// TODO: also need to have pictures clickable to take you to music page
-// TODO: add links to clickable buttons to visit song release
-// !Fix transitions of pictures!
-// ?PLAN: I may switch auto carousel idea to scrollable slide show with animation to opaque album art section with a button to switch to music page and create a full out music page
 
 
 
 
 export default function Home() {
 
-    // const data = [ aeona, revelation, brokenMachine, keepOn, aeona, revelation]
-    //     const [ currentIndex, setCurrentIndex ] = useState(0)
-    //        const scroll = () => {
-    //             if(currentIndex === data.length-1) {
-    //                 return setCurrentIndex(0)
-    //             } else {
-    //                 return setCurrentIndex(currentIndex+1)
-    //             }
-    //        }
-    //        useEffect(() => {
-    //         const interval = setTimeout(() => {scroll()}, 1000)
-    
-    //         return () => clearTimeout(interval)})
-    
-
-
+// !Need to connect button with links
 
     return (
         <div>
             <img className="bandHero" src={hero} alt="band picture" />
             <div className="newRelease">
-                <h2 className="newReleaseHeader">"KEEP ON" AVAILABLE NOW</h2>
-                <img className="newReleasePic" src={keepOn} alt="song picture" />
+                <h2 className="newReleaseHeader">"NEW HORIZON" AVAILABLE NOW</h2>
+                <img className="newReleasePic" src={NewHorizon} alt="song picture" />
                 <ul className="newReleaseBtn">
-                    <button className="rSpotifyBtn"><FontAwesomeIcon icon={faSpotify} size="2x" />Spotify</button>
+                    <a href="https://open.spotify.com/track/4x6dtVEq5MF5FA6n6SUZi9?si=5c3264eb26e34eaa" target="_blank"><button className="rSpotifyBtn"><FontAwesomeIcon icon={faSpotify} size="2x" />Spotify</button></a>
                     <button className="rAppleBtn"><FontAwesomeIcon icon={faApple} size="2x" />Music</button>
                     <button className="rAmazonBtn"><FontAwesomeIcon icon={faAmazon} size="2x" />Amazon Music</button>
                     <button className="rYoutubeBtn"><FontAwesomeIcon icon={faYoutube} size="2x" />Youtube</button>
                 </ul>
             </div>
-                
-                {/* Music Carousel will go here */}
-                        {/* Line 60 under music-container { data.map((item, index) => {
-                            return (
-                                <img className="single-art" style={{transform: `translate(-${currentIndex}%)`}} src={item} alt="photo" />
-                            ) 
-                        })} */}
-                        {/* !LEft OFF HERE */}
             <div id="music-slider">
                 <h2 className="music-header"> Music </h2>
                 <div className="music-slideTrack">
-                    <div className="music-slideItem">
-                        <img className="single-art" src={aeona} alt="revelation" />
+                    <div>
+                        <a href="/music"> <img className="single-art" src={aeona} alt="Aeona" /> </a>
                     </div>
-                    <div className="music-slideItem">
-                        <img className="single-art" src={revelation} alt="revelation" />
+                    <div>
+                        <a href="/music"> <img className="single-art" src={brokenMachine} alt="Broken Machine" /> </a>
                     </div>
-                    <div className="music-slideItem">
-                        <img className="single-art" src={brokenMachine} alt="revelation" />
+                    <div>
+                        <a href="/music"> <img className="single-art" src={keepOn} alt="Keep On" /> </a>
                     </div>
-                    <div className="music-slideItem">
-                        <img className="single-art" src={keepOn} alt="revelation" />
+                    <div>
+                        <a href="/music"> <img className="single-art" src={revelation} alt="revelation" /> </a>
                     </div>
-                    <div className="music-slideItem">
-                        <img className="single-art" src={aeona} alt="revelation" />
+                    <div>
+                        <a href="/music"> <img className="single-art" src={NewHorizon} alt="NewHorizon" /> </a>
                     </div>
-                    <div className="music-slideItem">
-                        <img className="single-art" src={revelation} alt="revelation" />
+                    <div>
+                        <a href="/music"> <img className="single-art" src={aeona} alt="Aeona" /> </a>
                     </div>
-                    <div className="music-slideItem">
-                        <img className="single-art" src={brokenMachine} alt="revelation" />
+                    <div>
+                        <a href="/music"> <img className="single-art" src={brokenMachine} alt="Broken Machine" /> </a>
                     </div>
-                    <div className="music-slideItem">
-                        <img className="single-art" src={keepOn} alt="revelation" />
+                    <div>
+                        <a href="/music"> <img className="single-art" src={keepOn} alt="Keep On" /> </a>
                     </div>
-                    <div className="music-slideItem">
-                        <img className="single-art" src={aeona} alt="revelation" />
+                    <div>
+                        <a href="/music"> <img className="single-art" src={revelation} alt="revelation" /> </a>
                     </div>
-                    <div className="music-slideItem">
-                        <img className="single-art" src={revelation} alt="revelation" />
+                    <div>
+                        <a href="/music"> <img className="single-art" src={NewHorizon} alt="NewHorizon" /> </a>
                     </div>
                 </div>
             </div>
         </div>
     )
 }
-{/* under inner-music <ul className="music-container">
-    <div className="music-item">
-        <img className="single-art" src={revelation} alt="revelation" />
-    </div>
-    <div className="music-item">
-        <img className="single-art" src={brokenMachine} alt="brokenMachine" />
-    </div>
-    <div className="music-item">
-        <img className="single-art" src={keepOn} alt="keepOn" />
-    </div>
-</ul> */}
--- src/pages/meetBand/meetBand.js
+++ src/pages/meetBand/meetBand.js
@@ -1,39 +1,45 @@
 import React from "react";
 import "./meetBand.css";
-import hero from "../../images/ECHomepage.jpg";
-import David from "../../images/David.jpeg";
-import Sarina from "../../images/Sarina.jpg";
-import Erik from "../../images/Erik.jpg";
-import Diego from "../../images/Diego.jpeg";
+import hero from "../../images/meetBand.JPG";
+import David from "../../images/david.JPG";
+import Sarina from "../../images/sarina.JPG";
+import Erik from "../../images/erik.JPG";
+import Diego from "../../images/diego.JPEG";
+import Victor from "../../images/victor.JPEG";
 
 export default function meetBand() {
 
     return (
         <div>
+          <img className="bandBackground" src={hero} alt="band image" />
 
           <h1 className="header">Biography</h1>
 
           <p className="bio-description">Setting no limitations on inspirational fuel, San Diego Rock Band Elysian Cadence came into the scene in 2020 with there single release “AEONA”. Bringing to the table their individual musical tastes ranging from Blues, Soul, Reggae, Funk, Punk-Rock, Post-Metalcore to Orchestral music to create the fusion music that trademarks the Elysian Cadence sound. </p>
 
-          <img className="bandBackground" src={hero} alt="band image" />
 
-          <h1 className="members">Members</h1>
+          <h1 className="members">Band Members</h1>
 
           <h3 className="nameR">David Rios</h3>
-          <p className="nameR">Bass & Lead Vocals</p>
+          <p className="titleR">Bass & Lead Vocals</p>
           <img className="profilepicR" src={David} alt="member pic" />
 
-          <h3 className="nameL">Sarina Rios</h3>
-          <p className="nameL">Lead Vocals</p>
+          <h3 className="nameLS">Sarina Rios</h3>
+          <p className="titleLS">Lead Vocals</p>
           <img className="profilepicL" src={Sarina} alt="member pic" />
 
-          <h3 className="nameR">Erik Garcia</h3>
-          <p className="nameR">Lead Guitar & Vocals</p>
+          <h3 className="nameRE">Erik Garcia</h3>
+          <p className="titleRE">Lead Guitar & Vocals</p>
           <img className="profilepicR" src={Erik} alt="member pic" />
 
           <h3 className="nameL">Diego Espinoza</h3>
-          <p className="nameL">Rhythm Guitar & Vocals</p>
+          <p className="titleL">Rhythm Guitar & Vocals</p>
           <img className="profilepicL" src={Diego} alt="member pic" />
+
+          <h3 className="nameRV">Victor Palavicini</h3>
+          <p className="titleRV">Drums</p>
+          <img className="profilepicR" src={Victor} alt="member pic" />
+
 
 
         </div>
